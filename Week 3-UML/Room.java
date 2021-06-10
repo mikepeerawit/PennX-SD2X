2dc82a417e671853f254f79881962c33f739fb0f
Peerawit Chariyawongsiri <michael.chry@gmail.com>
2021-06-10 18:46:44 +0700

    Add files via upload

---
+++ Week 3-UML/Room.java
@@ -0,0 +1,13 @@
+
+public class Room extends MapSite {
+	public int roomNumber;
+	public MapSite[] mapsites;
+	public Wall[] sides;
+	
+	public Wall getSide(int side) {
+		return null;
+	}
+	
+	public void setSide(int n, Wall wall) {}
+
+}
